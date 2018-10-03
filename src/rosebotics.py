22cdfafc7cb6262ba8aea06c05b2e42df49726ee
gaoq <gaoq@rose-hulman.edu>
2018-10-03 09:10:35 -0400

    change m3 name

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -26,6 +26,11 @@ class Snatch3rRobot(object):
     def stop(self,stop_action=StopAction.BRAKE.value):
         self.left_wheel.stop_spinning(stop_action)
         self.right_wheel.stop_spinning(stop_action)
+    def turn(self,angle,left_wheel_duty_cycle_percent,right_wheel_duty_cycle_percent=0):
+        while self.left_wheel.get_degrees_spun() & self.right_wheel.get_degrees_spun() <= angle:
+            self.go(left_wheel_duty_cycle_percent,right_wheel_duty_cycle_percent)
+            
+
 
 class Wheel(object):
     def __init__(self, port, default_duty_cycle_percent=100,
